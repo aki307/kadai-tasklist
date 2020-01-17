5ca785d4554d9b72e7bd2c5666bc77c742da0b76
aki <agosight0307@gmail.com>
2020-01-17 08:38:38 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -11,11 +11,18 @@
             {!! Form::model($task, ['route' =>'tasks.store']) !!}
             
             <div class="form-group">
+                {!! Form::label('status', '進捗:') !!}
+                {!! Form::text('status',null,['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
                 {!! Form::label('content', 'タスク:') !!}
                 {!! Form::text('content',null, ['class' => 'form-control']) !!}
                 
-                {!! Form::submit('決定') !!}
             </div>
+            {!! Form::submit('決定', ['class' => 'btn btn-primary']) !!}
+            
+        {!! Form::close() !!}
         </div>
     </div>
 
